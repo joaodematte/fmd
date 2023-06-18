4b13dc3067e35b9c4b41e6ccfe65e953880485ea
João Dematte <jgdematte@gmail.com>
2023-06-18 16:51:39 -0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSameLine: false,
+  semi: true,
+  quoteProps: 'consistent'
+};
